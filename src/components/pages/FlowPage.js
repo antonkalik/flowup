25d11a89c67571fa30b070517268f3ae5938ad73
Anton Kalik <antonkalik@gmail.com>
2018-07-31 23:41:56 +0300

    added more controls for box, include hovers

--- src/components/pages/FlowPage.js
+++ src/components/pages/FlowPage.js
@@ -16,59 +16,56 @@ export class FlowPage extends Component {
     }
 
     addBox() {
-        this.state.boxes.push(
-            this.paper.rect(20, 20, 90, 50, 4).attr({
-                fill: '#fff',
-                stroke: 0,
-                cursor: 'grab'
-            })
-        )
-        this.dragElements()
-    }
-
-    dragElements() {
-        let { boxes } = this.state
-
-        const dragger = function() {
+        const drag = function() {
             this.ox = this.type == 'rect' ? this.attr('x') : this.attr('cx');
             this.oy = this.type == 'rect' ? this.attr('y') : this.attr('cy');
+            this.animate({stroke: 'blue', 'stroke-width': 2}, 500);
+            circles.show()
         }
 
         const move = function(dx, dy) {
             let mx = this.ox + dx;
             let my = this.oy + dy;
 
-            const attributes = this.type == 'rect' ? {
-                x: mx,
-                y: my
-            } : {
-                cx: mx,
-                cy: my
-            };
+            const attributes = this.type == 'rect' ? { x: mx, y: my } : { cx: mx, cy: my };
 
             this.attr(attributes);
 
-            const attrContr1 = {
+            const attr1 = {
                 cx: this.attr('x'),
                 cy: this.attr('y') + 25
             }
 
-            const attrContr2 = {
-                cx: this.attr('x') + 90,
+            const attr2 = {
+                cx: this.attr('x') + 120,
                 cy: this.attr('y') + 25
             }
             
-            circles[0].attr(attrContr1);
-            circles[1].attr(attrContr2);
+            circles[0].attr(attr1)
+            circles[1].attr(attr2)
         }
 
-        const elements = this.paper.set([...boxes])
-        const circles = this.paper.set([
-            this.paper.circle(20, 45, 3).attr({fill: 'blue', stroke: 0}),
-            this.paper.circle(110, 45, 3).attr({fill: 'blue', stroke: 0})
-        ])
+        const up = function () {
+            this.animate({stroke: 0, 'stroke-width': 0}, 500);
+            circles.hide()
+        }
 
-        elements.drag(move, dragger);
+        this.state.boxes.push(
+            this.paper.rect(20, 20, 120, 50, 4).attr({
+                fill: '#fff',
+                stroke: 0,
+                cursor: 'grab'
+            })
+        )
+
+        const elements = this.paper.set([...this.state.boxes])
+
+        const circles = this.paper.set([
+            this.paper.circle(20, 45, 3),
+            this.paper.circle(140, 45, 3)
+        ]).attr({fill: 'blue', stroke: 0})
+
+        elements.drag(move, drag, up);
     }
 
     
@@ -83,4 +80,3 @@ export class FlowPage extends Component {
         )
     }
 }
-
