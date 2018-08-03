32bf3b3c788e4a02d96c2e8176ed27879f9d5ac0
Anton Kalik <antonkalik@gmail.com>
2018-08-03 20:29:49 +0300

    splited component box

--- src/components/pages/FlowPage.js
+++ src/components/pages/FlowPage.js
@@ -1,55 +1,28 @@
 import React, { Component } from 'react'
 import { Tools } from '../'
-import Draggable, {DraggableCore} from 'react-draggable';
+import Draggable from 'react-draggable';
+import Box from '../parts/Box'
 
 export class FlowPage extends Component {
     constructor(props) {
         super(props)
+        this.handleRef = React.createRef()
         this.paper = null
         this.state = { boxes: [], boxSelected: false }
     }
 
     selectItem() {
+        console.log(this.handleRef)
         this.setState({boxSelected: true})
     }
 
     addBox() {
-        const box = 
-            <Draggable
-                key={this.state.boxes.length}
-                handle=".handle"
-                defaultPosition={{x: 20, y: 20}}
-                position={null}
-                onStart={this.handleStart}
-                onDrag={this.handleDrag}
-                onStop={this.handleStop}
-            >
-                <div onClick={e => this.selectItem(e)} className={'handle'}>
-                    <div className={'box'}>
-                        <div className={'boxHead'}>
-                            <h3>Init</h3>
-                        </div>
-                        <div className={'boxBody'}>
-                            <div className={'boxIn'}>
-                                In
-                            </div>
-                            <div className={'BoxOut'}>
-                                Out
-                            </div>
-                        </div>
-                        <div className={'boxFooter'}>
-                            <input onChange={() => {}} value={'10'} />
-                        </div>
-                    </div>
-                </div>
-            </Draggable>
-
         this.setState(prevState => ({
-            boxes: [...prevState.boxes, box]
+            boxes: [...prevState.boxes, <Box key={this.state.boxes.length} />]
         }))
     }
     
-    render() {
+    render() {        
         return (
             <div className={'flowpage'}>
                 <Tools addbox={() => this.addBox()} />
@@ -63,9 +36,9 @@ export class FlowPage extends Component {
                             !this.state.boxes ? <p>No elements</p> : this.state.boxes.map(el => el)
                         }
                     </div>
-                    {
-                        this.state.boxSelected ? <div id={'panel'} style={{width: 200}} /> : <div id={'panel'} style={{width: 0}} />
-                    } 
+                        {
+                            this.state.boxSelected ? <div id={'panel'} style={{width: 200}} /> : <div id={'panel'} style={{width: 0}} />
+                        }
                 </div>
             </div>
         )
